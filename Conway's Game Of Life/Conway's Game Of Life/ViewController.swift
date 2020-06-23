8f656b5ab5c5601b741e2e83527f08dd52004c50
Wyatt Harrell <wyattaharrell@gmail.com>
2020-06-23 13:54:57 -0400

    Initial Xcode project created

---
+++ Conway's Game Of Life/Conway's Game Of Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conway's Game Of Life
+//
+//  Created by Wyatt Harrell on 6/23/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
