556b8f0979c6c1993eaa68f8f0a9fe792c0f507a
Wyatt Harrell <wyattaharrell@gmail.com>
2020-06-23 23:53:29 -0400

    Canges to GameBoard

--- Conway's Game Of Life/Conway's Game Of Life/Models/GameBoard.swift
+++ Conway's Game Of Life/Conway's Game Of Life/Models/GameBoard.swift
@@ -1,5 +1,5 @@
 //
-//  World.swift
+//  GameBoard.swift
 //  Conway's Game Of Life
 //
 //  Created by Wyatt Harrell on 6/23/20.
@@ -7,41 +7,3 @@
 //
 
 import Foundation
-
-public class World {
-    public var cells: [Cell] = []
-    public let size: Int
-
-    public init(size: Int) {
-        self.size = size
-
-        for x in 0..<size {
-            for y in 0..<size {
-                let randomState = arc4random_uniform(3)
-                let cell = Cell(x: x, y: y, state: randomState == 0 ? .alive : .dead)
-                cells.append(cell)
-            }
-        }
-    }
-
-    public func updateCells() {
-        var updatedCells = [Cell]()
-        let liveCells = cells.filter { $0.state == .alive }
-
-        for cell in cells {
-            let livingNeighbors = liveCells.filter { $0.isNeighbor(to: cell) }
-            switch livingNeighbors.count {
-            case 2...3 where cell.state == .alive:
-                updatedCells.append(cell)
-            case 3 where cell.state == .dead:
-                let liveCell = Cell(x: cell.x, y: cell.y, state: .alive)
-                updatedCells.append(liveCell)
-            default:
-                let deadCell = Cell(x: cell.x, y: cell.y, state: .dead)
-                updatedCells.append(deadCell)
-            }
-        }
-
-        cells = updatedCells
-    }
-}
